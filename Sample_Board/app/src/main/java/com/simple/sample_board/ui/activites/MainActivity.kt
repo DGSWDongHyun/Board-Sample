9338afd02e0490ea447b14f7395546d3485bc285
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2021-02-15 02:09:48 +0900

    [ADD] init project

---
+++ Sample_Board/app/src/main/java/com/simple/sample_board/ui/activites/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.simple.sample_board.ui.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simple.sample_board.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
